f1885abb3eaab5fe28846900b44528f034e8acd1
Larry Ruckman <ruckman@slac.stanford.edu>
2019-12-05 12:35:18 -0800

    upgrading to surf@v2.0.0.

--- firmware/targets/AmcCarrierLlrfBsaMpsMsgRx/ruckus.tcl
+++ firmware/targets/AmcCarrierLlrfBsaMpsMsgRx/ruckus.tcl
@@ -9,5 +9,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 loadRuckusTcl $::env(TOP_DIR)/submodules
 
 # Load target's source code and constraints
-loadSource      -dir  "$::DIR_PATH/hdl/"
-loadConstraints -dir  "$::DIR_PATH/hdl/"
+loadSource      -dir  "$::DIR_PATH/hdl"
+loadConstraints -dir  "$::DIR_PATH/hdl"
+
+# # Set the AppCore to amc_carrier_core at the end of target's ruckus.tcl file
+# set_property library amc_carrier_core [get_files AppCore.vhd]
